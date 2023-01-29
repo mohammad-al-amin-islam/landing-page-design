6dc623d0c209e70e57cdf88b773f4831e602b49f
mohammad-al-amin-islam <alamin.islambcpsc@gmail.com>
2023-01-29 11:44:43 +0600

    some design added

--- src/pages/home/ConsultantArea/ConsultantArea.js
+++ src/pages/home/ConsultantArea/ConsultantArea.js
@@ -2,13 +2,23 @@ import React from "react";
 import dr from "../../../assets/d1.png";
 import vect from "../../../assets/Vector.png";
 import "./ConsultanArea.css";
-
+import icon from "../../../assets/eventicons/location.png";
 const ConsultantArea = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 justify-items-center items-center mt-8">
-      <div className="w-96 ">
-        <div className="bg-img-consultant">
+      <div className="w-96 relative ">
+        <div className="bg-img-consultant ">
           <img className="w-72 m-auto" src={dr} alt="" />
+        </div>
+        <div>
+          <button className="btn btn-ghost border gap-2 absolute top-0">
+            <img src={icon} alt="" />
+            Consultant online
+          </button>
+          <button className="btn btn-ghost border gap-2 absolute bottom-0 right-0 shadow-sm bg-white">
+            <img src={icon} alt="" />
+            Consultant online
+          </button>
         </div>
       </div>
       <div className="">
